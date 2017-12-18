e70c2d9c945ae37e4dd24714ecd22045d758c5f0
ArtikusHG <artiomoleynic@gmail.com>
2017-12-18 19:19:26 +0200

    Add Arch setup script

---
+++ Setup-Arch.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+echo "Installing openbox, feh and tint2"
+sudo pacman -S openbox feh tint2
+echo "Installing numix"
+sudo pacaur -S numix-gtk-theme
+echo "Setting up the dotfiles"
+mkdir ~/.config/openbox
+cp .fehbg ~/
+cp autostart ~/.config/openbox/
+cp .Xresources ~/
+sudo rm /usr/share/themes/Numix/openbox-3/themerc
+sudo cp themerc /usr/share/themes/Numix/openbox-3/themerc
+sudo rm /etc/X11/openbox/menu.xml
+sudo cp menu.xml /etc/X11/openbox
+cp wallpaper.jpg ~/
+echo "Installing xterm"
+sudo apt-get install xterm
+echo 'PS1="[\W] "' > ~/.bashrc
+echo "All done. Now, restart openbox and start the terminal emulator to see the difference :D"
